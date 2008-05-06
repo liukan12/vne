1cf460e0b6f66bba4b49bbf70c0d487178473e31
karasevpa <karasevpa@60124b6e-824b-0410-a204-ab2e215a433f>
2008-05-06 01:19:51 +0000

    added diagram of VNE operation, base solution file, project VNE for the main code and VNE Data for data (graphics, diagrams, txt, etc)

---
+++ trunk/VNE_SOURCE/vne/vne.cpp
@@ -0,0 +1,18 @@
+/* The file containing the main() function
+** Launches the program, initializes things,
+** Sets into the main loop of display , check events, display, ..
+*/
+#include <stdio.h>
+#include <iostream>
+
+using std::cout;
+
+int main( int argc, char* argv[] )
+{
+
+	cout<<"Welcome to VNE 0.0! \n";
+
+	return 0;
+
+
+}
